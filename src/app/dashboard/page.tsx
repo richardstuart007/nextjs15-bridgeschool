0c56c6eefdf08646aac8aea7d6cca4fd742caeaa
richard stuart <richardstuart007@hotmail.com>
2024-12-28 10:38:00 +1300

    remove charts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
-import SummaryGraphs from '@/src/ui/dashboard/dashboard/summary/summary'
-import { SummarySkeleton } from '@/src/ui/dashboard/dashboard/summary/skeleton'
-import { Suspense } from 'react'
+// import SummaryGraphs from '@/src/ui/dashboard/dashboard/summary/summary'
+// import { SummarySkeleton } from '@/src/ui/dashboard/dashboard/summary/skeleton'
+// import { Suspense } from 'react'
 
 export default async function Page() {
   return (
     <main className='h-screen flex flex-col p-2 md:p-4'>
       <div className='flex-grow'>
-        <Suspense fallback={<SummarySkeleton />}>
+        {/* <Suspense fallback={<SummarySkeleton />}>
           <SummaryGraphs />
-        </Suspense>
+        </Suspense> */}
       </div>
     </main>
   )
