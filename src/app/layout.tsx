830fa2ac6359f57e89034a1b1190ce7b8749f8be
richard stuart <richardstuart007@hotmail.com>
2024-12-28 09:48:32 +1300

    project name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
     template: '%s | Bridge School Dashboard',
     default: 'Bridge School Dashboard'
   },
-  description: 'nextjs15upgrade01 Bridge School.',
-  metadataBase: new URL('https://nextjs15upgrade01-bridgeschool.vercel.app/')
+  description: 'nextjs15 Bridge School.',
+  metadataBase: new URL('https://nextjs15-bridgeschool.vercel.app/')
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
