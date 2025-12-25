01ae01f9201ed9951740c053dbafba291385f8b9
richard stuart <richardstuart007@hotmail.com>
2025-12-25 18:40:00 +1300

    eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import unusedImports from "eslint-plugin-unused-imports";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig([{
+    extends: [...nextCoreWebVitals],
+
+    plugins: {
+        "unused-imports": unusedImports,
+    },
+
+    rules: {
+        "unused-imports/no-unused-imports": "error",
+
+        "unused-imports/no-unused-vars": ["warn", {
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            argsIgnorePattern: "^_",
+        }],
+    },
+}]);
