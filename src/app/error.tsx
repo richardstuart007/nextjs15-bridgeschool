2fe07150547dea46b767378e2a13662729f10c01
richard stuart <richardstuart007@hotmail.com>
2026-02-18 18:39:55 +1300

    error.tsx and not-found.tsx moved to app folder and removed elsewhere

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,13 +11,15 @@ export default function Error({
   reset: () => void
 }) {
   useEffect(() => {
-    // Optionally log the error to an error reporting service
     console.error(error)
   }, [error])
 
   return (
-    <main className='flex h-full flex-col items-center justify-center'>
+    <main className='flex min-h-screen flex-col items-center justify-center'>
       <h2 className='text-center'>Something went wrong!</h2>
+      <p className='mt-2 text-sm text-gray-500'>
+        {process.env.NODE_ENV !== 'production' && error.message}
+      </p>
       <MyButton
         overrideClass='mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400'
         onClick={
